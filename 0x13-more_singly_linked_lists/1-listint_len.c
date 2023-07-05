947f2fc6887b4cd32cccb2edbc7f7697fec1b6b5
austin0x1A4 <karurimm@gmail.com>
2023-07-04 23:41:26 -0700

    1-mainc.

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include "lists.h"
+
+size_t listint_len(const listint_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
+
+    return count;
+}
+
