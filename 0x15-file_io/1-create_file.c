716d812d649147288fc6c9e439c5d3394977cb34
austin0x1A4 <karurimm@gmail.com>
2023-07-29 05:20:09 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,41 +1,33 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
 
 /**
  * create_file - Creates a file.
- * @filename: A pointer that points to the name of the file to create.
+ * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
- * Return: If the function fails, return -1.
- *         Otherwise return 1.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-    int fd;                /* File descriptor */
-    ssize_t bytesWritten;  /* Number of bytes written */
+	int fd, w, len = 0;
 
-    if (filename == NULL)
-        return -1; /* If filename is NULL, return -1 indicating an error */
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
-        return -1; /* If opening or creating the file fails, return -1 indicating an error */
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    if (text_content != NULL)
-    {
-        bytesWritten = write(fd, text_content, strlen(text_content));
-        if (bytesWritten == -1)
-        {
-            close(fd);
-            return -1; /* If writing to the file fails, return -1 indicating an error */
-        }
-    }
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-    close(fd);
-    return 1; /* Return 1 on success */
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
-
